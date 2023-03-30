18e6ecabfcfb7a14fdfabc971a5a185fd5fa87a9
Duncanwar <semunda9@gmail.com>
2023-03-30 17:59:22 +0200

    archive

---
+++ wp-content/themes/followAndre/template-parts/content-archive.php
@@ -0,0 +1,17 @@
+<div class="container">		
+<div class="post mb-5">
+				    <div class="media">
+					    <img class="mr-3 img-fluid post-thumb d-none d-md-flex" src="images/thumbs/2.jpg" alt="image">
+					    <div class="media-body">
+						    <h3 class="title mb-1"><a href="post.html">A Guide to Becoming a Full-Stack Developer</a></h3>
+						    <div class="meta mb-1"><span class="date">Published 3 months ago</span><span class="time">3 min read</span><span class="comment"><a href="#">26 comments</a></span></div>
+						    <div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
+						    <a class="more-link" href="post.html">Read more &rarr;</a>
+					    </div><!--//media-body-->
+				    </div><!--//media-->
+			    </div>
+<?php
+the_excerpt();
+?>
+
+</div>
